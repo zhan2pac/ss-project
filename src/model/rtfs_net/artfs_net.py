Fix torch.max in normalization

src/model/rtfs_net/artfs_net.py
@@ -161,4 +161,4 @@ class ARTFSNet(nn.Module):
         """
 
         tensor = tensor - torch.mean(tensor, dim=-1, keepdim=True)
-        return tensor / torch.max(torch.abs(tensor), dim=-1, keepdim=True)
+        return tensor / torch.max(torch.abs(tensor), dim=-1, keepdim=True)[0]

src/model/rtfs_net/rtfs_net.py
@@ -133,4 +133,4 @@ class RTFSNet(nn.Module):
         """
 
         tensor = tensor - torch.mean(tensor, dim=-1, keepdim=True)
-        return tensor / torch.max(torch.abs(tensor), dim=-1, keepdim=True)
+        return tensor / torch.max(torch.abs(tensor), dim=-1, keepdim=True)[0]